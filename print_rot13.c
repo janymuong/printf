77c81690b8e069176edfdb978618032eea9433eb
RobertRuai <robertruai4@gmail.com>
2023-01-26 06:45:40 -0800

    encodes a string into rot13

---
+++ print_rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_rot13 - main entry point
+  *
+  * Description - function that encodes a string into rot13
+  *
+  * @R: string to convert
+  *
+  * Return: size of output
+  */
+int print_rot13(va_list R)
+{
+	int i, j, count = 0;
+	char *r;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLM nopqrstuvwxyzabcdefghijklm";
+
+	r = va_arg(R, char *);
+	if (r == NULL)
+		r = "(null)";
+	for (j = 0; r[j] != '\0'; j++)
+	{
+		for (i = 0; in[i] != '\0'; i++)
+		{
+			if (r[j] == in[i])
+			{
+				_putchar(out[i]);
+				count++;
+				break;
+			}
+		}
+	}
+	return (count);
+}
