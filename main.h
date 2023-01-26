deaf63ba6e15b45f1638d6b51d0156ec14aeae2e
Jany Muong <me.roumuong@gmail.com>
2023-01-26 15:48:22 +0300

    update add main.h

--- main.h
+++ main.h
@@ -1,6 +1,32 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
+int _printf(const char *format, ...);
+int _print_char(va_list c);
+int _print_int(va_list i);
+int _print_dec(va_list d);
+int _print_bin(va_list b);
+int _print_string(va_list str);
+int _print_octal(va_list o);
+int _print_x(va_list x);
+int _print_X(va_list X);
+int _print_rev(va_list r);
+int _print_rot13(va_list R);
+int _print_unsigned(va_list u);
+
+/**
+  * struct format_specifier - format specifier structure for printf to select from
+  * @fp: pointer specifiers
+  *
+  * @f: pointer to he function for specifiers
+  */
+typedef struct format_specifier
+{
+	char *fp;
+	int (*f)(va_list);
+} f_spec;
 
 #endif /* printf */
